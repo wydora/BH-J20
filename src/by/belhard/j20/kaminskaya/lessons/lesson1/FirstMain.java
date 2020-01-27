1057b247a55801bc3b8ca5488e6f5c25744c5354
wydora <nikolay.vitko@gmail.com>
2020-01-27 21:05:43 +0300

    init

---
+++ src/by/belhard/j20/kaminskaya/lessons/lesson1/FirstMain.java
@@ -0,0 +1,10 @@
+package by.belhard.j20.kaminskaya.lessons.lesson1;
+
+public class FirstMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!!!111oneoneone");
+    }
+}
+
